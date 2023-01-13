c0eeb46a93c5b2e9991404a21ef220e2ae642959
Ritu Raj <rituraj201202@gmail.com>
2023-01-13 13:08:53 +0530

    database connection establised

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
@@ -8,12 +10,25 @@ app.use(cors());
 
 app.get("/", (req, res) => {
     return res.status(200).json({
-      message: "Server is up and running",
+        message: "Server is up and running",
     });
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, async () => {
+    mongoose.set("strictQuery", false);
+    await mongoose
+        .connect(process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log("Database Connected");
+        })
+        .catch((err) => {
+            console.log(`Database error >> ${err.toString()}`);
+        });
+    mongoose.Promise = global.Promise;
     console.log(`Server is running on port ${port}`);
 });
