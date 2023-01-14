5bc685852cb32ba286d9e0610478ba9a4f7a9279
Khushang Thakkar <thakkarkhushang@gmail.com>
2023-01-14 15:48:31 +0530

    trying a fix for file upload

--- Backend/app.js
+++ Backend/app.js
@@ -7,9 +7,6 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ limit: '50mb', extended: true}));
-
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
